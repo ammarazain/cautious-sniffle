de1b178b4404ab96f7ee490fa0bc17f55b6d7542
ammaraabbasi <52998025+ammarazain@users.noreply.github.com>
2020-12-24 14:03:52 +0500

    Add files via upload

---
+++ school.py
@@ -0,0 +1,9 @@
+user=input("ENTER YOUR LAST PASSING PERCENTAGE:")
+if user >'50':
+print("YOUR ENGLISH MARKS IS 75")
+print("YOUR MATH MARKS IS 25")
+print("YOUR URDU MARKS IS 50")
+print("YOUR ISLAMIAT MARKS IS 45")
+print("YOUR CHEMISTRY MARKS IS 50")
+else:
+print("you failed I think in one subject")
